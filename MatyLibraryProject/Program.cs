c8231c6eeba6c00303cdebd507fb74bad80fcabf
Greg Doud <gdoud@gmail.com>
2022-02-08 15:57:57 -0500

    Add project files.

---
+++ MatyLibraryProject/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace MatyLibraryProject {
+    internal class Program {
+        static void Main(string[] args) {
+
+            var a = Math.Add(1, 2);
+        }
+    }
+}
